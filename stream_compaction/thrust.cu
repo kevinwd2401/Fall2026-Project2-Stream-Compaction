6f3fc00af0fa06423516af9c5e4d588ef1ca2299
Kevin Du <kevinwd2401@gmail.com>
2026-09-14 00:25:40 -0400

    thrust scan

--- stream_compaction/thrust.cu
+++ stream_compaction/thrust.cu
@@ -22,6 +22,27 @@ namespace StreamCompaction {
             // TODO use `thrust::exclusive_scan`
             // example: for device_vectors dv_in and dv_out:
             // thrust::exclusive_scan(dv_in.begin(), dv_in.end(), dv_out.begin());
+            if (n == 0) {
+                timer().endGpuTimer();
+                return;
+			}
+
+            int* d_in, * d_out;
+            cudaMalloc(&d_in, n * sizeof(int));
+            cudaMalloc(&d_out, n * sizeof(int));
+			cudaMemcpy(d_in, idata, n * sizeof(int), cudaMemcpyHostToDevice);
+
+            auto dv_in = thrust::device_pointer_cast(d_in);
+            auto dv_out = thrust::device_pointer_cast(d_out);
+
+            thrust::exclusive_scan(dv_in, dv_in + n, dv_out);
+
+            cudaMemcpy(odata, d_out, n * sizeof(int), cudaMemcpyDeviceToHost);
+
+            cudaFree(d_in);
+			cudaFree(d_out);
+
+
             timer().endGpuTimer();
         }
     }
